3a30d76d2c218958575b3fb2d8e05e7414f71eb3
Aantek <antek14112003@gmail.com>
2023-11-01 22:49:48 +0100

    Progress
    TODO:
    $emit to pass reaction timer variable to display that properly styled.
    Overall styles should be improved imo

---
+++ reaction-timer-game/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container mx-auto text-center pt-10">
+    <h1 class="text-6xl py-10"> Reaction timer</h1>
+    <button @click="start" :disabled="isPlaying" class=" text-4xl rounded-full bg-slate-800 px-6 py-3 text-white ">Play</button>
+    <blockSection v-if="isPlaying" v-bind:delay="delay"/>
+  </div>
+</template>
+
+<script>
+
+import blockSection from './components/blockSection.vue'
+
+export default {
+  components: {
+    blockSection,
+  },
+  data(){
+    return{
+      isPlaying: false,
+      delay: null
+    }
+  },
+  methods: {
+    start(){
+      this.isPlaying = true
+      this.delay = 2000 + Math.random() * 5000
+    }
+  }
+}
+</script>
+
+<style>
+#app{
+  font-family: 'Courier New', Courier, monospace;
+}
+</style>
---
+++ reaction-timer-game/src/components/blockSection.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="w-full h-screen text-black font-extrabold text-4xl bg-green-400 flex justify-center items-center" id="sectionToClick" v-if="showBlock" @click="stopTimer">
+    Click me!
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['delay'],
+  data(){
+    return {
+      showBlock: false,
+      timer: null,
+      reactionTime: 0
+    }
+  },
+  mounted(){
+    //console.log(this.delay)
+    setTimeout(() => {
+      this.showBlock = true;
+      this.startTimer()
+    }, this.delay)
+  },
+  methods: {
+    startTimer(){
+      this.timer = setInterval(() => {
+        this.reactionTime += 10
+      }, 10)
+    },
+    stopTimer() {
+      clearInterval(this.timer)
+      console.log(this.reactionTime)
+    }
+  }
+ 
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#sectionToClick{
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
